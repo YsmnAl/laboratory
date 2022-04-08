71fcf6c353727462ca240dfb75803c9b12a726f3
YsmnAl <100358095+YsmnAl@users.noreply.github.com>
2022-04-08 20:41:09 +0300

    Create entrant.h

---
+++ KP_6/entrant.h
@@ -0,0 +1,15 @@
+#ifndef entrant_h
+#define entrant_h
+
+typedef struct
+{
+    char surname[80];
+    char initials[10];
+    char sex[1];
+    int school;
+    char medal[3];
+    int math;
+    int rus;
+    char essay[10];
+} entrant;
+#endif
