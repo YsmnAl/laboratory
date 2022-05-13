ee31be24a9667ce64e16a3406f1e65e167705760
YsmnAl <100358095+YsmnAl@users.noreply.github.com>
2022-05-14 02:52:47 +0300

    Create list.h

---
+++ KP_8/list.h
@@ -0,0 +1,34 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct _list_node list_node;
+typedef struct _list_node
+{
+    char data[100];
+    list_node *next;
+    list_node *prev;
+} list_node;
+
+typedef struct listIterator
+{
+    list_node *node;
+} listIterator;
+
+typedef struct
+{
+    list_node *head;
+} list;
+
+list *listCreate();
+void listPrint(list *l);
+void listInsert(list *lst, char *data);
+void listRemove(list *lst, char *data);
+int listLen(list *lst);
+void listInsertToK(list *lst, int k, char *data);
+listIterator *iteratorCreate(list *lst);
+void iteratorNext(listIterator *it);
+list_node *iteratorGet(listIterator *it);
+void iteratorSet(list_node *l, listIterator *it);
+void listInsertb(list *lst, char *data);
+
+#endif
